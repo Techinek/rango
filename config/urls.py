f64ed01a294858c20a1c47d534b0a67383e225fe
Andrey Kuskov <techinek@gmail.com>
2022-01-29 12:44:45 +0300

    Add first html template with a picture bound to /rango

--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('rango.urls'))
+    path('rango/', include('rango.urls'))
 ]
--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('Rango greets you, partner!')
+    context = {
+        'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'
+    }
+    return render(request, 'rango/index.html', context)
+
+
+def about(request):
+    return HttpResponse("Rango says here is the about page. <a href='http://127.0.0.1:8000/rango/'>Home</a>")
