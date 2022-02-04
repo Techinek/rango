14ea5c66f048ca095bc09895b4b6321e1709ad71
Andrey Kuskov <techinek@gmail.com>
2022-02-04 19:33:56 +0300

    Add custom user and a form for it

--- rango/models.py
+++ rango/models.py
@@ -1,5 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.template.defaultfilters import slugify
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile-images', blank=True)
+
+    def __str__(self):
+        return self.user.username
 
 
 class Category(models.Model):
--- some.py
+++ some.py
@@ -1,10 +1,15 @@
-arr = ['abcdde', 'baccd', 'eeabg']
-stones = []
-gemstones_count = set()
-
-for i in arr:
-    for j in set(i):
-        print(i)
+grades = [7, 27, 23, 65, 43, 0, 82, 0]
+results = []
+for i in grades:
+    if i > 37:
+        rounded = i + (5 - i) % 5
+        if (rounded - i) < 3:
+            results.append(rounded)
+        elif rounded >= 3:
+            results.append(i)
+    else:
+        results.append(i)
+print(results)
 
 
 
